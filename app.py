203a1e97b7914bf711c963410b17880aa44096e8
connor-r-bennett <118479089+connor-bennett@users.noreply.github.com>
2025-04-21 15:13:06 -0700

    fixed server bugs

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 # Imports
 from flask import Flask
 from flask import Flask, render_template, url_for
-from flask_bootstrap import Bootstrap5
-from image_info import image_info
+from flask_bootstrap import Bootstrap
 from PIL import Image, ImageOps 
+
 import os
 import random
 
-# create an instance of Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
-bootstrap = Bootstrap5(app)
+Bootstrap(app)
+
 
 # Routes
 @app.route('/')
 def hello():
-    shuffled = image_info[:]
-    random.shuffle(shuffled)
-    selected = shuffled[:3]
-    return render_template('index.html', images=selected)
+    return render_template('index.html')
