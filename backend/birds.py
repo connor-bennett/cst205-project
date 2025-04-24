8a1dbef5fb4ce8a9180c4c90bc5716c150402b7a
julian-men <ianmenjones@gmail.com>
2025-04-24 16:33:06 -0700

    readded birds.py somehow lost it

---
+++ backend/birds.py
@@ -0,0 +1,61 @@
+bird_data = {
+    "little_grebe.png": [
+        {
+            "english": "Little Grebe",
+            "german": "Zwergtaucher",
+            "scientific": "Tachybaptus ruficollis",
+            "location": "Germany, Berlin-Wilhelmsruh",
+            "size": "103k"
+        },
+        {
+            "english": "Little Grebe",
+            "german": "Zwergtaucher",
+            "scientific": "Tachybaptus ruficollis",
+            "location": "Germany, Berlin-Rosenthal",
+            "size": "104k"
+        }
+    ],
+    "Eurasian_Bittern.png": [
+        {
+            "english": "Bittern",
+            "german": "Rohrdommel",
+            "scientific": "Botaurus stellaris",
+            "location": "Germany, Müritz National Park",
+            "size": "158k"
+        }
+    ],
+    "corn_crake.png": [
+        {
+            "english": "Corncrake",
+            "german": "Wachtelkönig",
+            "scientific": "Crex crex",
+            "location": "Germany, Berlin-Rosenthal",
+            "size": "145k"
+        },
+        {
+            "english": "Corncrake",
+            "german": "Wachtelkönig",
+            "scientific": "Crex crex",
+            "location": "Germany, Berlin-Rosenthal",
+            "size": "308k"
+        }
+    ],
+    "black_tern.png": [
+        {
+            "english": "Black Tern",
+            "german": "Trauerseeschwalbe",
+            "scientific": "Chlidonias niger",
+            "location": "Germany, Mecklenburg-Vorpommern",
+            "size": "170k"
+        }
+    ],
+    "oystercatcher.png": [
+        {
+            "english": "Oystercatcher",
+            "german": "Austernfischer",
+            "scientific": "Haematopus ostralegus",
+            "location": "Germany, Amrum",
+            "size": "128k"
+        }
+    ]
+}
